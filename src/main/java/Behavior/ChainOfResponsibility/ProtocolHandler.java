9f8e8b10634dbca9da7ff9990bd2657ea485ca57
KANG JIHYO <luxuryjihyo@naver.com>
2023-03-22 09:59:03 +0900

    add : Chain of Responsibility

---
+++ src/main/java/Behavior/ChainOfResponsibility/ProtocolHandler.java
@@ -0,0 +1,13 @@
+package Behavior.ChainOfResponsibility;
+
+public class ProtocolHandler extends Handler{
+    @Override
+    protected void process(String url) {
+        int index = url.indexOf("://");
+        if(index != -1){
+            System.out.println("PROTOCOL : " + url.substring(0,index));
+        } else {
+            System.out.println("No Protocol");
+        }
+    }
+}
